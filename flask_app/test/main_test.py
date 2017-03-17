f8b84a3824925a46c2d1ee1acd495cbad53e1a83
sjh8543 <sjh8543@gmail.com>
2017-03-17 17:44:42 +0900

    add env value into Dockerfile and add routing_url function to main_test.py

--- flask_app/test/main_test.py
+++ flask_app/test/main_test.py
@@ -4,15 +4,15 @@ import unittest
 
 class FlaskrTestCase(unittest.TestCase):
     """
-     It's a main application test class for funtional testing. A|B testing engine can be devided by three modules 
-    which are controller, service and database access object. Each modules need to do unit testing and finally get a 80% code coverage rate.
-    Functional test that have been described and implemented in this class is a key to keep application quality high and will be used for load test.
+      It's a main application test class for funtional testing. A|B testing engine can be devided by three modules 
+     which are controller, service and database access object. Each modules need to do unit testing and finally get a 80% code coverage rate.
+     Functional test that have been described and implemented in this class is a key to keep application quality high and will be used for load test.
     """
     def setUp(self):
         """
          set up initialize steps before test begin like creating db connection or test client
         """
-        pass
+        self.app = main.app.test_client()
 
     def tearDown(self):
         """
@@ -20,11 +20,12 @@ class FlaskrTestCase(unittest.TestCase):
         """
         pass
     
-    def routing_test(self):
+    def test_routing(self):
         """
          function to test for routing url
         """
-        pass
+        rv = self.app.get('/routing')
+        assert "It's routing URL" in rv.data.decode('utf-8')
 
 if __name__ == '__main__' :
     unittest.main()
