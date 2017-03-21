a278c21cb0fd6267b973021b6eec159ddafffa2e
sjh8543 <sjh8543@gmail.com>
2017-03-21 17:20:24 +0900

    add test function for reporting, admin, collecting URL

--- flask_app/test/main_test.py
+++ flask_app/test/main_test.py
@@ -19,14 +19,34 @@ class FlaskrTestCase(unittest.TestCase):
          finalize step after whole tests are end
         """
         pass
-    
+
+    def test_admin(self):
+        """
+         It's test function for admin URL"
+        """
+        rv = self.app.get('/admin')
+        assert "It's admin URL" in rv.data.decode('utf-8')
+
+    def test_collecting(self):
+        """
+         It's test function for collecting URL"
+        """
+        rv = self.app.get('/collecting')
+        assert "It's collecting URL" in rv.data.decode('utf-8')
+
     def test_routing(self):
         """
-         function to test for routing url
+         It's test function for routing url
         """
         rv = self.app.get('/routing')
         assert "It's routing URL" in rv.data.decode('utf-8')
 
+    def test_reporting(self):
+        """
+         It's test function for reporting url
+        """
+        rv = self.app.get('/reporting')
+        assert "It's reporting URL" in rv.data.decode('utf-8')
+    
 if __name__ == '__main__' :
     unittest.main()
-
